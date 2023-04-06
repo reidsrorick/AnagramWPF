c8cd8cae736ddd1e9e69866927e9a18c7e3dad0f
reids <reids@DESKTOP-3E260UG>
2023-04-06 12:37:00 -0500

    Add project files.

---
+++ Anagrams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramWPF
+{
+    internal class Anagrams
+    {
+        public List<string> OriginalWord { get; set; }
+
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
+
+namespace AnagramWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string word;
+        private string selectedWord;
+        public MainWindow()
+        {
+            InitializeComponent();
+            lblPercentage.Visibility = Visibility.Hidden;
+            lblShown.Visibility = Visibility.Hidden;
+            lblTotal.Visibility = Visibility.Hidden;
+            lblTime.Visibility = Visibility.Hidden;
+            gif.Visibility = Visibility.Hidden;
+            //var gifPath = "C:\\Users\\reids\\OneDrive\\Desktop\\Code\\AnagramWPF\\Loading.gif";
+            //var gifUri = new Uri(gifPath);
+            //gif.Source = gifUri;
+            //gif.LoadedBehavior = MediaState.Manual;
+            //gif.Stop();
+
+        }
+
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            lstbxAnagrams.Items.Clear();
+
+            word = txtWord.Text;
+            //if (word.Length > 5)
+            //{
+            //      gif.Visibility = Visibility.Visible;
+            //    gif.Play();
+            //}
+            string specify = txtSpecification.Text;
+            DateTime startTime = DateTime.Now;
+            List<string> anagrams = GenerateAnagrams(word);
+            double count = 0;
+            double anagramcount = anagrams.Count;
+            foreach (string anagram in anagrams)
+            {
+                if (anagram.Substring(0, specify.Length) == specify)
+                {
+                    lstbxAnagrams.Items.Add(anagram);
+                    count++;
+                }
+
+            }
+            //gif.Visibility = Visibility.Hidden;
+            lblShown.Content = $"Anagrams Shown: {count.ToString("#,##0")}";
+            lblTotal.Content = $"Total iterations for your word: {anagramcount.ToString("#,##0")}";
+            double percentage = (count / anagramcount);
+            lblPercentage.Content= $"Percentage of anagrams shown: {percentage.ToString("P2")}";
+            TimeSpan difference = DateTime.Now - startTime;
+            lblTime.Content = $"Runtime: {difference.TotalSeconds.ToString("N2")} seconds";
+            lblShown.Visibility = Visibility.Visible;
+            lblTotal.Visibility = Visibility.Visible;
+            lblTime.Visibility = Visibility.Visible;
+            lblPercentage.Visibility = Visibility.Visible;
+        }
+        static List<string> GenerateAnagrams(string word)
+        {
+            List<string> anagrams = new List<string>();
+            if (word.Length == 1)
+            {
+                anagrams.Add(word);
+            }
+            else
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    string word1 = word.Substring(0, i);
+                    string word2 = word.Substring(i + 1);
+                    string remaining = word1 + word2;
+                    List<string> subAnagrams = GenerateAnagrams(remaining);
+                    foreach (string subAnagram in subAnagrams)
+                    {
+                        anagrams.Add(word[i] + subAnagram);
+                    }
+                }
+            }
+            return anagrams;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "Anagrams.csv";
+            string newInput = $"{word},{selectedWord}";
+            List<string> strings= new List<string>();
+            strings.Add("Original, Anagram");
+            string[] file = System.IO.File.ReadAllLines(path);
+            foreach (string line in file)
+            {
+                strings.Add(line);
+            }
+            strings.Add(newInput);
+            System.IO.File.WriteAllLines(path, strings.ToArray());
+            
+        }
+
+        private void lstbxAnagrams_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedWord = lstbxAnagrams.SelectedItem.ToString();
+        }
+    }
+}
